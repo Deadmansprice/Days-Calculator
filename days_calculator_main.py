7f2905809a2528de557aee17de26d37e47f2969d
Deadmansprice <18153828+Deadmansprice@users.noreply.github.com>
2023-12-18 21:13:18 +1100

    version 1.0 - Completed project.
    Added in date comparison class for comparing number of days, months
    and years between 2 dates provided by the user.
    
    Updated the while loops to be only 1, removing 2nd while loop
    making this more efficient.
    
    Added in prints, showing days, months and years.
    
    Added in main menu and used while loops to allow user to confirm
    if they want to calculate the above between both dates and to
    close off, as necessary. Double checked the error messages and
    they look good.
    
    Because of this I only needed to keep it to 1 file.

--- days_calculator_main.py
+++ days_calculator_main.py
@@ -1,13 +1,9 @@
-from datetime import datetime, date
-import os, calendar
-
-todays_date = date.today().strftime("%d %B %Y")
-
+from datetime import datetime
+import os, calendar, time
+from dateutil import relativedelta
 
 standard_year_days = 365
 Leap_year_days = 366
-
-print(f"Today's date is {todays_date}")
 
 class date_set():
 
@@ -41,8 +37,8 @@ class date_set():
         #It will be first year regardless.
         while counter < 2:
             while True:
-                year = int(input("What year? Please 4 digits only as we're using Gregorian calendar system\n"))
-                if year <= 0:
+                year = int(input("What year? Please 4 digits only as we're using Gregorian calendar system in AD.\n"))
+                if year < 0:
                     print("You cannot go below 1!")
                     continue
                 else:
@@ -52,7 +48,10 @@ class date_set():
             #For user input for the first set of date
             while True:
                 month = int(input("What month do you want to set it to? Numerical value please, as in '1' for 'January'\n"))
-                if month <= 12:
+                if month < 1:
+                    print("You cannot go below 1!")
+                    continue
+                elif month <= 12:
                     print("Within 12 months, moving to next step")
                     break
                 else:
@@ -65,7 +64,7 @@ class date_set():
                 if day > date_set.last_day_of_month(year, month):
                     print("You cannot set the date beyond the last day of this month.")
                     continue
-                elif day <= 0:
+                elif day < 1:
                     print("You cannot go below 1!")
                     continue
                 else:
@@ -75,21 +74,58 @@ class date_set():
             counter +=1
     
             if counter == 1: 
+                global first_date #necessary for date comparison
                 first_date = datetime(year, month, day)
                 month_covert_to_string = date_set.int_to_month_conv(month) #Calls int_to_month_conv function within date_set class to convert the int provided by user to string within month dictionary.
                 first_date_print = f"Your first date has been set: {day} {month_covert_to_string} {year}"
+                print(first_date_print)
+                time.sleep(1)
+                os.system('cls')
             elif counter == 2: 
+                global second_date #necessary for date comparison
                 second_date = datetime(year, month, day)
+                print(first_date_print)
                 if second_date < first_date:
                     print("Your second date cannot be less than the first date!")
                     counter = 0
                     continue
                 else:
                     second_date_print = f"Your second date has been set: {day} {month_covert_to_string} {year}"
-               
+                
                 print(first_date_print)
                 print(second_date_print)
 
 
+class date_comparison:
+    def compare_dates():
+        #pulls in global variable that would be shown in the set_date class.
+        date_delta = relativedelta.relativedelta(first_date,second_date)
+        day_delta = second_date - first_date
 
-date_set.set_date()
+        print(f"The difference between the dates are in {abs(date_delta.years)} years, {abs(date_delta.months)} months, {abs(date_delta.weeks)} weeks, {abs(date_delta.days)} days")
+        print(f"In total number of months: {abs(date_delta.years * 12)}") #number of months in year. so 12 x however many years.
+        print(f"In total number of weeks: {abs(date_delta.years * 52)}") #number of whole weeks. 
+        print(f"In total number of days: {abs(day_delta.days)}") #calculates number of days between two dats.
+        print(f"In total number of days, including end date: {abs(day_delta.days + 1)}")
+        
+class main_menu:
+    def main_menu():
+        user_confirmation = input("Would you like to compare dates?\n")
+        while user_confirmation.strip().lower() == 'y':
+            date_set.set_date()
+            date_comparison.compare_dates()
+            user_confirmation_continue = input("Continue? y for yes, n for no only.\n")
+            if user_confirmation_continue.strip().lower() == 'y':
+                print('Continuing')
+                time.sleep(2)
+                os.system('cls')
+                continue
+            elif user_confirmation_continue.strip().lower() == 'n':
+                print('exiting')
+                exit()
+            else: 
+                print('No other input allowed. Continuing')
+                continue
+
+
+main_menu.main_menu()
